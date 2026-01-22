d47c15d89be5eb0ea1baa1ff63ce7a44f7e6995b
Daniel Kec <daniel.kec@oracle.com>
2026-01-22 14:50:21 +0100

    update

--- src/main/java/net/dmitrykornilov/helidon/assistant/rag/EmbeddingModelFactory.java
+++ src/main/java/net/dmitrykornilov/helidon/assistant/rag/EmbeddingModelFactory.java
@@ -5,7 +5,7 @@ import java.util.function.Supplier;
 import io.helidon.service.registry.Service;
 
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.bgesmallenv15q.BgeSmallEnV15QuantizedEmbeddingModel;
 
 /**
  * Creates EmbeddingModel as a service
@@ -14,7 +14,7 @@ import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2Quantize
 public class EmbeddingModelFactory implements Supplier<EmbeddingModel> {
     @Override
     public EmbeddingModel get() {
-        return new AllMiniLmL6V2QuantizedEmbeddingModel();
+        return new BgeSmallEnV15QuantizedEmbeddingModel();
     }
 
 }
