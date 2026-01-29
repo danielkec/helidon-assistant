55ed6dc8908c0e0f969716833d0290f89b651783
Daniel Kec <daniel.kec@oracle.com>
2026-01-29 13:43:17 +0100

    Trigger missing config mapping gen

--- buildHelidonSnapshot.sh
+++ buildHelidonSnapshot.sh
@@ -6,7 +6,10 @@ mkdir ${DIR}
 cd ${DIR}
 git clone -b kec/agentic-poc --single-branch git@github.com:danielkec/helidon.git
 cd helidon
+
 mvn install -T4 -DskipTests -Dmaven.javadoc.skip=true
+# Trigger config docs generation ...
+cd docs && mvn package && cd ..
 
 printf "app:
  helidon-repo-path: $(pwd)
